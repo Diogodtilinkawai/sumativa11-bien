5b9dc2ed484957f152d93ae09ad4a595812e6d51
Donvicio <10143@portalcedi.edu.mx>
2025-02-25 08:58:07 -0600

    generador de ideas 1

--- sumativa11/src/app/home/home.page.ts
+++ sumativa11/src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Router} from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { OpenaiService } from '../openai.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule,IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonItem,IonLabel,IonInput,IonCard,IonCardHeader,IonCardContent,IonCardTitle],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router:Router,private OpenaiService:OpenaiService ) {}
+
+  ideaPrompt:string = '';
+  generatedIdea:string = '';
+
+  async generateIdea(){
+    if(this.ideaPrompt.trim()==='')
+    {
+      alert('Por favor ingrese una idea');
+      return;
+    }
+    this.generatedIdea = await this.OpenaiService.generateIdea(this.ideaPrompt);
+
+  }
+
+
 }
---
+++ sumativa11/src/app/openai.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { max } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenaiService {
+  private apikey:string = 'VM6bYVrsBmV34PQ3twvq3NsRZPMsIdLnPiWKmgaM'; 
+
+  constructor() { }
+  async generateIdea(prompt:string): Promise<string>{
+    const url = 'https://api.openai.com/v1/engines/davinci/completions';
+    try {
+      const response = await axios.post(url, {
+        model : 'gpt-3.5-turbo',
+        messages: [
+          {role: 'system', content: 'Eres un generador de ideas'},
+          {role: 'user', content: prompt} 
+        ],
+        max_tokens: 100,
+        temperature: 0.7,
+      }, {
+        headers: {
+          'Authorization':`Bearer. ${this.apikey}`,
+          'content-type': 'application/json'
+        }
+      });
+      console.log('Respuesta de openai:',response.data);
+      if (response.data && response.data.choices && response.data.choices[0].message )
+        {
+        return response.data.choices[0].message.content.trim();
+      }
+      else{
+        return 'No se encontró respuesta';
+      }
+    }  
+    catch (error){
+      console.error('Error en openai:', error);
+      return 'No se encontró respuesta';
+    }
+  }
+}
+  
